dde9860942baeb4fe1b2f91bd5bf870f54950e68
Tibor Ignéczi <sidragxsam@gmail.com>
2021-05-19 22:27:36 +0200

    Test added

--- src/main/java/org/openjfx/Model/BMICalc.java
+++ src/main/java/org/openjfx/Model/BMICalc.java
@@ -4,6 +4,22 @@ public class BMICalc {
     private double magassag;
     private double suly;
 
+    public double getMagassag() {
+        return magassag;
+    }
+
+    public void setMagassag(double magassag) {
+        this.magassag = magassag;
+    }
+
+    public double getSuly() {
+        return suly;
+    }
+
+    public void setSuly(double suly) {
+        this.suly = suly;
+    }
+
     public BMICalc(double suly, double magassag) {
         this.magassag = magassag;
         this.suly = suly;
---
+++ src/main/java/org/openjfx/Test/BMICalcTest.java
@@ -0,0 +1,46 @@
+package org.openjfx.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openjfx.Model.BMICalc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalcTest {
+    private BMICalc undertest;
+
+    @BeforeEach
+    void setup(){
+        undertest = new BMICalc(68, 170);
+    }
+
+    @Test
+    void tesztBMIOsztas(){
+        assertEquals(23.529, undertest.szamitBMI());
+    }
+
+    @Test
+    void getMagassag() {
+        double m = undertest.getMagassag();
+        assertEquals(170, m);
+    }
+
+    @Test
+    void setMagassag() {
+        undertest.setMagassag(175);
+        assertEquals(175,undertest.getMagassag());
+    }
+
+    @Test
+    void getSuly() {
+        double s = undertest.getSuly();
+        assertEquals(68, s);
+    }
+
+    @Test
+    void setSuly() {
+        undertest.setSuly(55);
+        assertEquals(55,undertest.getSuly());
+    }
+
+}
